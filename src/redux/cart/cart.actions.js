2776599df9c663bd9255f19988c0fea874c4624a
Lee <jjisanle@gmail.com>
2020-12-31 19:04:12 +0900

    added cartIcon and cartDropdown copmonents to our application, also implementd card reducer

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,46 @@
+import CartActionTypes from './cart.types';
+
+export const toggleCartHidden = () => ({
+    type : CartActionTypes.TOGGLE_CART_HIDDEN
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
