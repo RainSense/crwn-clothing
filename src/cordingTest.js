afcfb25d44d18b2184c569f349c7974cca419309
Lee <jjisanle@gmail.com>
2021-01-02 19:10:06 +0900

    updating our application with reselect cart selectors with memoized selectors

---
+++ src/cordingTest.js
@@ -0,0 +1,22 @@
+var lengthOfLongestSubstring = function(s) {
+    
+    
+    let subSet = [];
+    
+
+    for(let i = 0; i<s.length; i++) {
+
+        if(!subSet.includes(s.charAt(i))) {
+            subSet[i] = s.charAt(i);
+        }
+
+    }
+
+    s.length;
+    
+
+
+    
+    
+};
+
